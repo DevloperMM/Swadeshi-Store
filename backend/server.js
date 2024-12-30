c27c28e8d49c91a5803293a4ea53cb80921a6f8d
DevloperMM <mangalmurtivarshney2810@gmail.com>
2024-12-30 11:25:42 +0530

    MongoDB and Redis connection setup

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
 
-dotenv.config();
+dotenv.config({ path: "./.env" });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
+  connectDB();
 });
