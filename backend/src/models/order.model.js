f0271a3a96ffcc62ea048a20ce9d2434f6594bfa
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-01-03 01:21:48 +0530

    Integrated payment gateway backend structure

---
+++ backend/src/models/order.model.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    buyer: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    products: [
+      {
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+          min: 1,
+        },
+      },
+    ],
+    orderTotal: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    paymentId: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,7 @@ import userRoutes from "./routes/user.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
+import paymentRoutes from "./routes/payment.route.js";
 
 dotenv.config();
 
@@ -21,6 +22,7 @@ app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
 app.use("/api/v1/coupons", couponRoutes);
+app.use("/api/v1/payments", paymentRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
