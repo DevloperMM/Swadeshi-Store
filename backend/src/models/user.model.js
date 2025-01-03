19266ff78c6a112fd11de211562f5df49aa6b06a
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-01-03 11:36:30 +0530

    Added analytics for admin to get data insights

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -63,4 +63,6 @@ userSchema.methods.generateRefreshToken = function () {
   });
 };
 
-export const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema);
+
+export default User;
