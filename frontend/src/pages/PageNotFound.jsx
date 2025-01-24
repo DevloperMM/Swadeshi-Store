232c6d6decbb9540bd1399fb5ed1a9d95c1cc182
DevloperMM <mangalmurtivarshney2810@gmail.com>
2025-01-24 19:12:54 +0530

    Bug@page_title Fix#04

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -4,9 +4,9 @@ function PageNotFound() {
   return (
     <div className="relative min-h-screen text-white overflow-hidden">
       <div className="relative z-10 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <h4 className="text-center text-xl font-bold text-emerald-400 mb-4">
+        <h3 className="text-center text-2xl sm:text-3xl font-bold text-emerald-400 mb-4">
           404 Error
-        </h4>
+        </h3>
         <h2 className="text-center text-3xl sm:text-5xl font-bold text-emerald-400 mb-4">
           Page Not Found
         </h2>
